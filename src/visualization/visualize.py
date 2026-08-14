8d2516df585c451fb71de9a6489f44608428b0db
Disha Kwatra <dishakwatra01@gmail.com>
2026-08-15 04:11:53 +0530

    feat(viz): add combined plots for accelerometer and gyroscope data by exercise and participant

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -131,8 +131,46 @@ for label in labels:
 # Combine plots in one figure
 # --------------------------------------------------------------
 
+label = "row" #exercise name
+participant = "A"
+combined_plot_df = (
+    df.query(f"label == '{label}'")
+    .query(f"participant == '{participant}'")
+    .reset_index(drop=True) #we drop the index
+)
 
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20,10))
+combined_plot_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0])
+combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+#styling
+ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+ax[1].set_xlabel("samples")
 
 # --------------------------------------------------------------
 # Loop over all combinations and export for both sensors
 # --------------------------------------------------------------
+
+labels = df["label"].unique()
+participants = df["participant"].unique()
+
+for label in labels:
+    for participant in participants:
+        combined_plot_df = (
+            df.query(f"label == '{label}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+        
+        if len(combined_plot_df) > 0:
+            
+            fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20,10))
+            combined_plot_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0])
+            combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+            #styling
+            ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+            ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+            ax[1].set_xlabel("samples")
+            
+            plt.savefig(f"../../reports/figures/{label.title()} ({participant}).png")
+            plt.show()
